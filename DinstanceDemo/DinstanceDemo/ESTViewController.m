976cc652c7de28f4368dd2ef051d1532e99a5bc0
Dana Petersen <dana.petersen@gmail.com>
2013-10-21 21:16:35 -0500

    Made this shit work

--- DinstanceDemo/DinstanceDemo/ESTViewController.m
+++ DinstanceDemo/DinstanceDemo/ESTViewController.m
@@ -12,17 +12,22 @@
 #define DOT_MIN_POS 150
 #define DOT_MAX_POS screenHeight - 70;
 
-@interface ESTViewController () <ESTBeaconManagerDelegate>
+@interface ESTViewController () <CLLocationManagerDelegate>
 
-@property (nonatomic, strong) ESTBeaconManager* beaconManager;
+//@property (nonatomic, strong) ESTBeaconManager* beaconManager;
+
+@property (nonatomic, strong) CLLocationManager* locationManager;
+
 @property (nonatomic, strong) UIImageView*      positionDot;
 
 @property (nonatomic) float dotMinPos;
 @property (nonatomic) float dotRange;
+@property (weak, nonatomic) IBOutlet UILabel *uiRssi;
 
 @end
 
 @implementation ESTViewController
+@synthesize uiRssi = _uiRssi;
 
 - (void)viewDidLoad
 {
@@ -32,16 +37,25 @@
     // setup Estimote beacon manager
     
     // create manager instance
-    self.beaconManager = [[ESTBeaconManager alloc] init];
-    self.beaconManager.delegate = self;
-    self.beaconManager.avoidUnknownStateBeacons = YES;
+   // self.beaconManager = [[ESTBeaconManager alloc] init];
+   // self.beaconManager.delegate = self;
+  //  self.beaconManager.avoidUnknownStateBeacons = YES;
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
     
     // create sample region object (you can additionaly pass major / minor values)
-    ESTBeaconRegion* region = [[ESTBeaconRegion alloc] initRegionWithIdentifier:@"EstimoteSampleRegion"];
+   // ESTBeaconRegion* region = [[ESTBeaconRegion alloc] initRegionWithIdentifier:@"EstimoteSampleRegion"];
+  //  NSUUID *proximityUUID = [[NSUUID alloc] initWithUUIDString:@"7BD3E324-D9CE-1518-EC79-5823A34E73A1"];
+    NSUUID *proximityUUID = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6F"];
+    CLBeaconRegion* region = [[CLBeaconRegion alloc] initWithProximityUUID:proximityUUID identifier:@"Dana Region"];
+
     
     // start looking for estimtoe beacons in region
     // when beacon ranged beaconManager:didRangeBeacons:inRegion: invoked
-    [self.beaconManager startRangingBeaconsInRegion:region];
+  //  [self.beaconManager startRangingBeaconsInRegion:region];
+    
+    [self.locationManager startRangingBeaconsInRegion:region];
     
     
     /////////////////////////////////////////////////////////////
@@ -55,28 +69,28 @@
     /////////////////////////////////////////////////////////////
     // setup background image
     
-    CGRect          screenRect          = [[UIScreen mainScreen] bounds];
-    CGFloat         screenHeight        = screenRect.size.height;
-    UIImageView*    backgroundImage;
-    
-    if (screenHeight > 480)
-        backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundBig"]];
-    else
-        backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundSmall"]];
-    
-    [self.view addSubview:backgroundImage];
-    
-    /////////////////////////////////////////////////////////////
-    // setup dot image
-    
-    self.positionDot = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"dotImage"]];
-    [self.positionDot setCenter:self.view.center];
-    [self.positionDot setAlpha:1.];
-    
-    [self.view addSubview:self.positionDot];
-    
-    self.dotMinPos = 150;
-    self.dotRange = self.view.bounds.size.height  - 220;
+//    CGRect          screenRect          = [[UIScreen mainScreen] bounds];
+//    CGFloat         screenHeight        = screenRect.size.height;
+//    UIImageView*    backgroundImage;
+//    
+//  //  if (screenHeight > 480)
+//  //      backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundBig"]];
+//  //  else
+//  //      backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundSmall"]];
+//    
+//  //  [self.view addSubview:backgroundImage];
+//    
+//    /////////////////////////////////////////////////////////////
+//    // setup dot image
+//    
+//    self.positionDot = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"dotImage"]];
+//    [self.positionDot setCenter:self.view.center];
+//    [self.positionDot setAlpha:1.];
+//    
+//    [self.view addSubview:self.positionDot];
+//    
+//    self.dotMinPos = 150;
+//    self.dotRange = self.view.bounds.size.height  - 220;
 }
 
 
@@ -98,6 +112,36 @@
     }
 }
 
+//- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+//
+//    CLBeacon *closestBeacon = [locations objectAtIndex:0];
+//
+//}
+
+- (void) locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    
+    if([beacons count] > 0)
+    {
+        CLBeacon *closestBeacon = [beacons objectAtIndex:0];
+        // beacon array is sorted based on distance
+        // closest beacon is the first one
+        
+        // calculate and set new y position
+        float newYPos = self.dotMinPos + ((float)closestBeacon.rssi / -100.) * self.dotRange;
+        self.positionDot.center = CGPointMake(self.view.bounds.size.width / 2, newYPos);
+        
+        //NSLog (@"Current date: %@", [NSDate date]);
+        self.uiRssi.text = [NSString stringWithFormat:@"%d", closestBeacon.rssi ];
+
+        NSLog(@"Beacon: %@", closestBeacon);
+    }
+}
+
+- (void) locationManager:(CLLocationManager *)manager rangingBeaconsDidFailForRegion:(CLBeaconRegion *)region withError:(NSError *)error
+{
+    NSLog(@"Got error");
+    
+}
 
 - (void)didReceiveMemoryWarning
 {
